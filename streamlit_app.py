80e1fc70250cfcc274771db65434d0e7ddbaf463
rabiyaasif <121342268+rabiyaasif@users.noreply.github.com>
2024-08-08 15:18:50 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import pandas as pd
-import numpy as np
+
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
